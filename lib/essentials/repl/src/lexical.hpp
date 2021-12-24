c8d004f19c9321466acbdcf3da4e2d7de99597b2
Mizan-Al-Sabbih <cmizanurrahman77@gmail.com>
2021-12-24 08:30:57 -0500

    Create lexical.hpp

---
+++ lib/essentials/repl/src/lexical.hpp
@@ -0,0 +1,15 @@
+#ifndef LEXICAL_HPP
+#define LEXICAL_HPP
+
+enum class lexer {
+    STRING = -1,
+    INTEGER = -2,
+    OPERATOR = -3,
+    PRINT = -4,
+    VARIABLE = -5,
+    LET = -6,
+    STATIC = -7,
+    ENUM = -8
+};
+
+#endif
